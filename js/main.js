c65407e1266ec5886833ecec00237507cbb18352
edwardmccormick <66527737+edwardmccormick@users.noreply.github.com>
2020-11-05 16:53:18 -0600

    Becoming a javascript WIZ

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// "use strict";
+// console.log("Hello from the Javascript Console!");
+// console.log("Hello from the magical world of keyboard shortcuts!");
+//
+// alert("You just won a new iPad!");
+// alert("Just kidding, of course, email and zip code, please!");
+//
+// var iPodWinner = "You just  won an iPod 5G";
+//
+// alert(iPodWinner);
+//
+// var confirmed = confirm("Are you sure you want to do xyz?")
+// console.log(confirmed);
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
