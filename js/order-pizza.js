3c353d6b53ae1171b26a929c4be4aa3654217034
edwardmccormick <ted.mccormick@gmail.com>
2020-12-02 00:47:34 -0600

    Finalizing work - late night!

---
+++ js/order-pizza.js
@@ -0,0 +1,28 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+
+    // const togglePassword = document.querySelector('#togglePassword');
+    // const password = document.querySelector('#password');
+    // togglePassword.addEventListener('click', function (e) {
+    //     // toggle the type attribute
+    //     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    //     password.setAttribute('type', type);
+    //     // toggle the eye slash icon
+    //     this.classList.toggle('fa-eye-slash');
+
+})();
