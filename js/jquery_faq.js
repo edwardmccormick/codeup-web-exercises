48c64871f383fc9bf7499879408f0989b202767f
edwardmccormick <ted.mccormick@gmail.com>
2021-01-05 19:09:02 -0600

    finished with Ajax work

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -6,7 +6,8 @@ $('.answer, ul').hide(0);
 $('document').ready(function() {
 $('#visibilitytoggle').click(function(e) {
     e.preventDefault();
-    $('dd').slideToggle(1000)
+    $('dd').slideToggle(1000);
+    $('ul').slideToggle(1000);
 })
 $('dt').click(function() {
     $(this).toggleClass('highlight');
@@ -42,5 +43,18 @@ $('h3').click(function() {
 })
 
 $('h2').delay(8000).fadeIn(1000)
+
+$('.pictureButton').click(function() {
+    if ($(this).parent().hasClass("left")) {
+        $(this).parent().toggleClass('left middle');
+        $(this).parent().next().toggleClass('left middle')
+    }
+    if ($(this).parent().hasClass('right')) {
+        $(this).parent().toggleClass('right middle')
+        $(this).parent().prev().toggleClass('right middle')
+        console.log("The right side is registering!")
+    }
+
+    })
 
 })
